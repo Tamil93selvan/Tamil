b4649a74d089ee892e44dfcdd24fb1339f54c9ec
Tamil <151004738+Tamil93selvan@users.noreply.github.com>
2023-11-28 20:10:49 +0530

    Add files via upload

---
+++ hypotenou.js
@@ -0,0 +1,15 @@
+let a;
+let b;
+let c;
+
+ a = window.prompt("Enter side A");
+ a = Number(a);
+
+ b = window.prompt("Enter side B");
+ a = Number(b);
+
+ c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+ console.log("side c:", c);
+
+ 3
---
+++ typeof.js
@@ -0,0 +1,11 @@
+let x;
+let y;
+let z;
+
+x = Number("3.33");
+y = String(45);
+z = Boolean("Tamil");
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z,typeof z);
